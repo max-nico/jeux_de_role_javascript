598f5fe72384e36c0c967817ad98df33947664a8
Nicolas <thugnic@gmail.com>
2019-11-22 14:02:00 +0100

    select perso init game

--- javascript/game.js
+++ javascript/game.js
@@ -4,11 +4,12 @@ import  {bubu, dylan, flash} from "./personico.js";
 //console.log(flash);console.log(dylan);console.log(bubu);
 //console.log(perso1, perso_3_max, perso_1_max, perso_2_max);
 
-let Tabperso = [flash,dylan,bubu,perso_1_max,perso_3_max,perso_2_max];
+let Tabperso = [perso_1_max,perso_2_max,perso_3_max,bubu,dylan,flash];
 let selectedPerso = [];
+let choose = document.getElementById('choose-perso');
+
 
 function SelectPerso (){
-    let choose = document.getElementById('choose-perso');
     Tabperso.forEach(element => {
         choose.innerHTML += `<input type="checkbox" id="_${element.id}">${element.name}</input>`;
     });
@@ -19,13 +20,52 @@ function SelectPerso (){
         cb.addEventListener("click", function(e){
             if (selectedPerso.length < 2) {
                 selectedPerso.push(cb.id); 
-                console.log(selectedPerso); 
             } else {
-                e.preventDefault(); // arrete le onclick et le push des persos dans le tableau
+                //si plus de 2 choix effacer derniere entrer dans le tableau selected et l'id
+                let index = selectedPerso.indexOf(e.target.id);
+                if (index > -1) {
+                    selectedPerso.splice(index, 1);
+                }
+                // Stop selected checkbox 
+                if (selectedPerso.length==2) {
+                    e.preventDefault(); 
+                }
             }
         })  
     }
 }
+function validechoice() {
+    let btn = document.getElementById('valide');
+    btn.addEventListener("click", function(){
+        console.log('click');
+        
+        for (let i=selectedPerso.length-1; i >= 0; i--) {
+            let indexperso = parseInt(selectedPerso[i].replace("_","") -1);
+                console.log('tabperso selceted ' + Tabperso[indexperso].name);
+                let ul = document.createElement("ul");
+                choose.append(ul);
+                ul.innerHTML+= `<li>${Tabperso[indexperso].name}</li>`;
+                ul.innerHTML+= `<li>${Tabperso[indexperso].race}</li>`;
+                ul.innerHTML+= `<li>${Tabperso[indexperso].quality}</li>`;
+                ul.innerHTML+= `<li>${Tabperso[indexperso].victory}</li>`;
+                ul.innerHTML+= `<li>${Tabperso[indexperso].loose}</li>`;
+                ul.innerHTML+= `<li>${Tabperso[indexperso].degats}</li>`;
+                ul.innerHTML+= `<li>${Tabperso[indexperso].pv}</li>`;
+                Tabperso[indexperso].img = "./assets/" + Tabperso[indexperso].id + ".png";
+                ul.innerHTML+= `<li><img src="${Tabperso[indexperso].img}"></li>`;
+                ul.innerHTML+= `<li>${Tabperso[indexperso].block}</li>`;
+                
+            }
+    })
+}
+
+function initgame(params) {
+
+    SelectPerso();
+    validechoice();
+
+}
+
+initgame();
 
 
-SelectPerso();
--- javascript/personico.js
+++ javascript/personico.js
@@ -2,19 +2,20 @@ import Personnage from "./players.js";
 
 const divblock = document.createElement("DIV");
 divblock.classList.add('block-perso');
-//let url
-let img = document.createElement("img");
-//imgbubu.setAttribute("src", url);
 
 
 
 
-export let bubu = new Personnage(4,'Bubu', 'Tortue', 'Lente','C.....est......moi.......qu....ai.........gagné','ahhhh.......je........meurs',6 ,24, img, divblock);
 
 
 
-export let dylan = new Personnage(5,'Johnny', 'Raton-laveur', 'Voleur','bah guette que jvoyons comment que jlai baggarrer','Le sang de mes morts que j ai perdu' ,4 ,30, img, divblock);
+
+export let bubu = new Personnage(4,'Bubu', 'Tortue', 'Lente','C.....est......moi.......qu....ai.........gagné','ahhhh.......je........meurs',6 ,24, "", divblock);
 
 
 
-export let flash = new Personnage(6,'Furtif', 'Ecureil', 'Gland','je t ai bien eu..','rien' ,6 ,22, img, divblock);
+export let dylan = new Personnage(5,'Johnny', 'Raton-laveur', 'Voleur','bah guette que jvoyons comment que jlai baggarrer','Le sang de mes morts que j ai perdu' ,4 ,30, "", divblock);
+
+
+
+export let flash = new Personnage(6,'Furtif', 'Ecureil', 'Gland','je t ai bien eu..','rien' ,6 ,22, "", divblock);
