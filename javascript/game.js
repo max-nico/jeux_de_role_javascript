9a2f09fe744b8ca01a42256f43d1b9294d7a0d0f
Nicolas <thugnic@gmail.com>
2019-11-22 10:46:59 +0100

    creation de class perso Nico 3 persos

--- javascript/game.js
+++ javascript/game.js
@@ -1,5 +1,5 @@
 import Personnage from "./players.js";
-
+import Personnage from "./personico.js";
 let perso1 = new Personnage('jojo le castor', 'castor', 'mignon','hihi','ahhhh',2 ,6,"","");
 
 console.log(perso1);
---
+++ javascript/personico.js
@@ -0,0 +1,22 @@
+import Personnage from "./players.js";
+
+const divblock = document.createElement("DIV");
+divblock.classList.add('block-perso');
+//let url
+let img = document.createElement("img");
+//imgbubu.setAttribute("src", url);
+
+
+
+
+let bubu = new Personnage('Bubu', 'Tortue', 'Lente','C.....est......moi.......qu....ai.........gagné','ahhhh.......je........meurs',6 ,24, imgbubu, divblock);
+
+console.log(bubu);
+
+let dylan = new Personnage('Johnny', 'Raton-laveur', 'Voleur','bah guette que jvoyons comment que jlai baggarrer','Le sang de mes morts que j ai perdu' ,4 ,30, img, divblock);
+
+console.log(dylan);
+
+let flash = new Personnage('Furtif', 'Ecureil', 'Gland','je t ai bien eu..','rien' ,6 ,22, img, divblock);
+
+console.log(flash);
