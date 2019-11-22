d86769cd7537f440663fd0d9936963e96f1bd0ef
Nicolas <thugnic@gmail.com>
2019-11-22 10:28:48 +0100

    creation de class perso

--- javascript/game.js
+++ javascript/game.js
@@ -1,1 +1,5 @@
-import {*} from 'players.js';
+import Personnage from "./players.js";
+
+let perso1 = new Personnage('jojo le castor', 'castor', 'mignon','hihi','ahhhh',2 ,6,"","");
+
+console.log(perso1);
---
+++ javascript/players.js
@@ -0,0 +1,17 @@
+
+export default class Personnage{
+    constructor (name, race, quality, victory, loose, degats, pv, img, block){
+
+        this.name = name;
+        this.race = race;
+        this.quality = quality;
+        this.victory = victory;
+        this.loose = loose;
+        this.degats = degats;
+        this.pv = pv;
+        this.img = img;
+        this.block = block;
+    }
+
+
+}
